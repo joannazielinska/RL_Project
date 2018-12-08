abe0f9a5b95befa841690e7971ac37074eed59c4
LaunoisJB <45490583+LaunoisJB@users.noreply.github.com>
2018-12-08 20:01:24 +0200

    Runnable DQN with experience replay
    Need to add the save and load of models, to optimize and comment it.
    Not tested yet, so no information on its learning ability

--- DQN/agent.py
+++ DQN/agent.py
@@ -1,5 +1,10 @@
 from pong import Pong
+import torch
 import torch.nn.functional as F
+import torch.optim as optim
+from collections import deque
+import random
+import numpy as np
 
 class Policy(torch.nn.Module):
     def __init__(self, state_space, action_space):
@@ -8,9 +13,9 @@ class Policy(torch.nn.Module):
         self.action_space = action_space
         
         #Layers
-        self.conv1 = torch.nn.Conv2d(state_space, 32, 8, stride=4)
-        self.conv2 = torch.nn.Conv2d(32, 64, 4, stride=2)
-        self.fc1 = torch.nn.Linear(64, 256)
+        self.conv1 = torch.nn.Conv2d(state_space, 32, kernel_size=8, stride=4, padding=0)
+        self.conv2 = torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0)
+        self.fc1 = torch.nn.Linear(11*11*64, 256)
         self.fc2 = torch.nn.Linear(256, action_space)
     
     def forward(self, x):
@@ -18,10 +23,10 @@ class Policy(torch.nn.Module):
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
+        x = x.view(-1,11*11*64)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        print(x.shape)
         return x
     
 
@@ -32,37 +37,43 @@ class Agent(object):
         self.env = env
         self.player_id = player_id
         self.bpe = 4
-        self.name = "DQN-AI-trial"
+        self.name = "DQN_AI"
         
         self.train_device = "cpu"  # ""cuda" if torch.cuda.is_available() else "cpu"
         self.policy = policy.to(self.train_device)
         
         self.batch_size = 1
         self.gamma = 0.98
+        
+        self.optimizer = optim.RMSprop(policy.parameters(), lr=0.0025, alpha=0.9, eps=1e-02, momentum=0.0)
     
-    def get_name():
+    def get_name(self):
         return self.name
 
-	def load_model(filename):
+    def load_model(self,filename):
 	    print("Loading")
     
-    def get_action(frame, epsilon=0.5):
+    def get_action(self, frame, epsilon=0.5):
         rnd = random.random()
         if rnd < 1-epsilon :
             x = torch.from_numpy(frame).float().to(self.train_device)
             q_values = self.policy.forward(x)
-            action = q_values.max(dim=0)
+            action = q_values.argmax()
             action = np.asscalar(action.data.to(self.train_device).numpy())
         else:
             action = random.randint(0,3)
         return action
     
-    def reset():
+    def update_policy(self):
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+    
+    def reset(self):
         print("Reset")
         
-class Memory(object)
+class Memory(object):
     def __init__(self, max_size=10000, batch_size=64):
-        self.data = deque(max_len=max_size)
+        self.data = deque(maxlen=max_size)
         self.batch_size = batch_size
         
     def store(self, outcome):
@@ -72,5 +83,5 @@ class Memory(object)
         index = random.sample(range(0,len(self.data)), self.batch_size)
         samples = []
         for i in index:
-            samples.append(self.data[index])
+            samples.append(self.data[i])
         return samples
--- DQN/pong_ai.py
+++ DQN/pong_ai.py
@@ -7,6 +7,8 @@ import argparse
 
 from skimage.transform import resize
 from collections import deque
+import random
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--headless", action="store_true", help="Run in headless mode")
@@ -17,8 +19,11 @@ Parameters
 '''
 episodes = 10
 epsilon = 0.5
+gamma = 0.9
 decay = 0.00001
-memory_size = 10000
+memory_size = 100#10000
+batch_size = 64
+update_step = 1000
 
 
 def plot(observation):
@@ -31,9 +36,13 @@ Preprocessing the frame the reduce the load on system :
     - resize it to half
 '''
 def preprocess(frame):
-    frame[frame != 0] = 1
-    frame = resize(frame, ((frame.shape[0]//2), (frame.shape[1]//2)))
-    return frame
+    processed_frame = np.zeros((frame.shape[0],frame.shape[1]))
+    for ix in range(frame.shape[0]):
+        for iy in range(frame.shape[1]):
+            if (frame[ix,iy].any != 0):
+                processed_frame[ix,iy] = 255
+    processed_frame = resize(processed_frame, ((frame.shape[0]//2), (frame.shape[1]//2)))
+    return processed_frame
 
 '''
 Stacking 4 frames. The stacked frames will be used for learning.
@@ -53,7 +62,12 @@ def stack_frame(stacked_frames, new_frame, reset=False):
         stacked_frames.append(frame)
     else :
         stacked_frames.append(frame)
-    state = np.stack(stacked_frames, axis=2)
+    
+    state = np.zeros((1,4,frame.shape[0],frame.shape[1]))
+    state[:,0] = stacked_frames[0]
+    state[:,1] = stacked_frames[1]
+    state[:,2] = stacked_frames[2]
+    state[:,2] = stacked_frames[2]
     return state, stacked_frames
 
 '''
@@ -67,8 +81,6 @@ player_id = 1
 opponent_id = 3 - player_id
 opponent = PongAi(env, opponent_id)
 
-env.set_names(player.get_name(), opponent.get_name())
-
 #Model
 action_space_dim = 3
 observation_space_dim = 4
@@ -76,24 +88,30 @@ observation_space_dim = 4
 #Classes
 policy = Policy(observation_space_dim, action_space_dim)
 player = Agent(env, policy, player_id)
+target_dqn = Policy(observation_space_dim, action_space_dim)
+target_dqn.load_state_dict(policy.state_dict())
 
+env.set_names(player.get_name(), opponent.get_name())
 
 #Stacked preprocessed frames
-stacked_frames = deque(np.zeros(200,210), maxlen=4)
+stacked_frames = deque(np.zeros((200,210)), maxlen=4)
+
+#Updates
+update_counter = 0
 
 #Memory Initialisation
 # take random actions to fill the memory
-memory = Memory(memory_size)
+memory = Memory(memory_size, batch_size)
 for i in range(memory_size):
     if (i==0):
         obs = env.reset()
         state, stacked_frames = stack_frame(stacked_frames, obs[0], True)
     action1 = random.randint(0,3)
     action2 = random.randint(0,3)
-    next_obs, reward, done, info = env.step((action1,action2))
+    next_obs, rewards, done, info = env.step((action1,action2))
     next_state, stacked_frames = stack_frame(stacked_frames, next_obs[0])
-    memory.store((next_state, action1, reward[0], done))
-    #state = next_state
+    memory.store((state, action1, rewards[0], next_state, done))
+    state = next_state
 
 '''
 Training
@@ -103,6 +121,8 @@ for i in range(0,episodes):
     done = False
     obs = env.reset()
     state, stacked_frames = stack_frame(stacked_frames, obs[0], True)
+    timesteps = 0
+    reward_sum = 0
     
     while not done:
         action1 = player.get_action(state, epsilon)
@@ -111,12 +131,55 @@ for i in range(0,episodes):
         next_obs, rewards, done, info = env.step((action1, action2))
         next_state, stacked_frames = stack_frame(stacked_frames, next_obs[0])
         
-        memory.store((next_state, action1, reward[0], done))
+        memory.store((state, action1, rewards[0], next_state, done))
+        reward_sum += rewards[0]
 
         obs = next_obs
         state = next_state
         
-    #Updating policy
+        env.render()
+        
+        #Updating policy
+            #Loading from memory
+        samples = memory.sample()
+        batch_states = np.asarray([x[0] for x in samples])
+        batch_actions = np.asarray([x[1] for x in samples])
+        batch_rewards = np.asarray([x[2] for x in samples])
+        batch_next_states = np.asarray([x[3] for x in samples])
+        batch_done = np.asarray([x[4] for x in samples])
+            
+            #Target network
+        batch = torch.from_numpy(batch_next_states.squeeze()).float().to(player.train_device)
+        batch_t_q_values = target_dqn.forward(batch)
+        
+            #Q Learning
+        batch_t_q_max,_ = batch_t_q_values.max(dim=1)
+        y = torch.empty(batch_size, 1)
+        batch_rewards = torch.from_numpy(batch_rewards).float().to(player.train_device)
+        
+        for j in range(batch_size):
+            #.any() ?
+            if batch_done[j].any():
+                y[j] = batch_rewards[j]
+            else:
+                y[j] = batch_rewards[j] + batch_t_q_max[j].mul(gamma)
+        y.detach()
+                
+            #Gradient_descent
+        batch_q_values = policy.forward(torch.from_numpy(batch_states.squeeze()).float().to(player.train_device))
+        loss = torch.mean(y.sub(batch_q_values)**2)
+        loss.backward()
+        
+        player.update_policy()
+        
+        update_counter += 1
+        if (update_counter % update_step == 0):
+            target_dqn.load_state_dict(policy.state_dict())
+        timesteps += 1
+        
+    epsilon = epsilon*decay
+    print("Episode {} finished. Total reward: {:.3g} ({} timesteps)"
+              .format(i, reward_sum, timesteps))
     
     
     
